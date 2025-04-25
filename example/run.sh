9844f6f2cd6d99a054d6bfc8d0df5619eca481f1
tuanloc1105 <tuanloc1105@gmail.com>
2025-04-25 13:54:26 +0700

    update pom file

--- example/run.sh
+++ example/run.sh
@@ -9,11 +9,20 @@ printf "\n\n >> Checking java version \n\n\n"
 
 java -version
 
+# java \
+#  -server \
+#  -Duser.timezone=Asia/Ho_Chi_Minh \
+#  -Dfile.encoding=UTF8 \
+#  -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -Xloggc:data/gc.log \
+#  -cp \
+#  target/example-1.0.0.jar:target/lib/*:classes \
+#  com.example.lcx.App
+
 java \
  -server \
  -Duser.timezone=Asia/Ho_Chi_Minh \
  -Dfile.encoding=UTF8 \
- -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -Xloggc:data/gc.log \
+ -Xlog:gc*:file=data/gc.log:time,uptime,level,tags:filecount=5,filesize=10m \
  -cp \
  target/example-1.0.0.jar:target/lib/*:classes \
  com.example.lcx.App
