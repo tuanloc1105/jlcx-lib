d7ec0ef31343782ad463c5e41aafeb807ca23e08
tuanloc1105 <tuanloc1105@gmail.com>
2025-06-28 19:46:20 +0700

    chore: update build-web.ps1 to manage directory context during build process

--- example/build-web.ps1
+++ example/build-web.ps1
@@ -1,5 +1,9 @@
+$currentLocation = $(Get-Location)
+
 Remove-Item -Recurse src\main\resources\webroot\* *> $null || Write-Host "`n`n`t No webroot folder`n`n`n"
 
 Set-Location web
 
 pnpm run build
+
+Set-Location $currentLocation
