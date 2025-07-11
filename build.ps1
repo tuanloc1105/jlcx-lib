5b36352e065d2d5d72ed24b349aff276068a1d06
tuanloc1105 <tuanloc1105@gmail.com>
2025-07-11 20:49:31 +0700

    fix: update build script to use correct syntax for Maven options

--- build.ps1
+++ build.ps1
@@ -10,4 +10,4 @@ Write-Host "`n`n`t Checking maven version`n`n`n"
 
 mvn --version
 
-mvn clean install "-DskipTests=true" "-Dfile.encoding=UTF8" -f pom.xml
+mvn clean install -D"skipTests=true" -D"file.encoding=UTF8" -f pom.xml
