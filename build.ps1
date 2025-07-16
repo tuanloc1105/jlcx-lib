c683c85517726fbb129573d3359e0d40a592fb0e
tuanloc1105 <tuanloc1105@gmail.com>
2025-07-16 09:39:46 +0700

    chore: update JAVA_HOME and MAVEN_HOME environment variables to use DEV_KIT_LOCATION

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 Clear-Host
 
-$env:JAVA_HOME  = "D:\dev-kit\jdk-11"
-$env:MAVEN_HOME = "D:\dev-kit\maven"
+$env:JAVA_HOME  = "$env:DEV_KIT_LOCATION\jdk-11"
+$env:MAVEN_HOME = "$env:DEV_KIT_LOCATION\maven"
 $env:PATH       = "$env:JAVA_HOME\bin;$env:MAVEN_HOME\bin;$env:PATH"
 
 Write-Host "`n`n`t Checking java version`n`n`n"
