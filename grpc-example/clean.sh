4bf0f24419b14817f67e6ded6537d8893d6957f7
tuanloc1105 <tuanloc1105@gmail.com>
2025-08-01 09:16:09 +0700

    chore: remove unused Greeter service files and update clean script to remove examples from both grpc-client and grpc-server

--- grpc-example/clean.sh
+++ grpc-example/clean.sh
@@ -7,4 +7,4 @@ rm -R $HOME/.m2/repository/com/example/grpc-server 2> /dev/null || echo "Not fou
 rm -R grpc-client/target                           2> /dev/null || echo "Not found grpc-client/target"
 rm -R grpc-server/target                           2> /dev/null || echo "Not found grpc-server/target"
 
-rm -Rf grpc-server/src/main/java/examples grpc-server/src/main/java/examples
+rm -Rf grpc-server/src/main/java/examples grpc-client/src/main/java/examples
