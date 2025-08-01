50fd4fd4cdd5894926c9a341fa81ff02ceec69e0
tuanloc1105 <tuanloc1105@gmail.com>
2025-08-01 15:41:03 +0700

    refactor: remove unused Base, Do, and Execute classes; update MyRouterImpl and ApplicationVerticle to use SLF4J for logging

--- common-lib/src/main/java/vn/com/lcx/common/test_pkg/Base.java
+++
@@ -1,7 +0,0 @@
-package vn.com.lcx.common.test_pkg;
-
-public abstract class Base {
-
-    public abstract void doSomething();
-
-}
--- common-lib/src/main/java/vn/com/lcx/common/test_pkg/Do.java
+++
@@ -1,8 +0,0 @@
-package vn.com.lcx.common.test_pkg;
-
-public class Do extends Base {
-    @Override
-    public void doSomething() {
-        System.out.println("OK");
-    }
-}
