3b6ce63565f9683f6e999d768c1e2fabff36569f
tuanloc1105 <tuanloc1105@gmail.com>
2025-08-16 14:50:54 +0700

    chore: update pom.xml files to remove jakarta.mail dependency and add maven-shade-plugin configuration; modify clean scripts to comment out node_modules removal

--- examples/todo-app-example/clean.ps1
+++ examples/todo-app-example/clean.ps1
@@ -2,4 +2,4 @@ Clear-Host
 
 Remove-Item -Recurse -Force "$env:USERPROFILE\.m2\repository\com\example\lcx" 2> $null ; if (!$?) { Write-Host "Not found m2\repository\com\example\lcx" }
 Remove-Item -Recurse -Force "target"                                          2> $null ; if (!$?) { Write-Host "Not found target" }
-Remove-Item -Recurse -Force web\node_modules                                  2> $null ; if (!$?) { Write-Host "Not found web\node_modules" }
+# Remove-Item -Recurse -Force web\node_modules                                  2> $null ; if (!$?) { Write-Host "Not found web\node_modules" }
