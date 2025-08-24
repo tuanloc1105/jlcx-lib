ef5b7eb1d98d06d1526f8fb0bfb1cb8b471fafc3
tuanloc1105 <tuanloc1105@gmail.com>
2025-08-24 18:02:56 +0700

    refactor: remove index-merge-plugin and related configurations from the project

--- clean.sh
+++ clean.sh
@@ -6,4 +6,3 @@ rm -R $HOME/.m2/repository/vn/com/lcx 2> /dev/null || echo "Not found m2/reposit
 rm -R common-lib/target               2> /dev/null || echo "Not found common-lib/target"
 rm -R target                          2> /dev/null || echo "Not found target"
 rm -R processor/target                2> /dev/null || echo "Not found processor/target"
-rm -R index-merge-plugin/target       2> /dev/null || echo "Not found index-merge-plugin/target"
