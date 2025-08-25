cb0de47fb7a42f0a09068947153cd00df89188aa
tuanloc1105 <tuanloc1105@gmail.com>
2025-08-25 07:48:16 +0700

    chore: add release and snapshot scripts for Maven deployment with Java and Maven version checks

---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+clear
+
+export JAVA_HOME="$HOME/dev-kit/jdk-11"
+export MAVEN_HOME="$HOME/dev-kit/maven"
+export PATH="$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH"
+
+printf "\n\n >> Checking java version \n\n\n"
+
+java -version 2>&1
+
+printf "\n\n >> Checking maven version \n\n\n"
+
+mvn --version
+
+mvn \
+  deploy \
+  -DaltDeploymentRepository=nexus::https://nexus.vtl.name.vn/repository/maven-releases/ \
+  -DskipTests=true \
+  -Dfile.encoding=UTF8 \
+  -f \
+  pom.xml
