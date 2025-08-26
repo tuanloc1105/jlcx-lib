872a5635fa8e1c6e4663c8ad85534cb92517dbaf
tuanloc1105 <tuanloc1105@gmail.com>
2025-08-26 13:13:40 +0700

    Update example

--- examples/todo-app-example/src/main/java/com/example/lcx/respository/TaskRepository.java
+++ examples/todo-app-example/src/main/java/com/example/lcx/respository/TaskRepository.java
@@ -4,6 +4,7 @@ import com.example.lcx.entity.TaskEntity;
 import io.vertx.core.Future;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.sqlclient.SqlConnection;
+import vn.com.lcx.common.database.pageable.Page;
 import vn.com.lcx.common.database.pageable.Pageable;
 import vn.com.lcx.reactive.annotation.Query;
 import vn.com.lcx.reactive.annotation.RRepository;
@@ -32,6 +33,14 @@ public interface TaskRepository extends ReactiveRepository<TaskEntity> {
             "    AND t.deleted_at IS NULL\n" +
             "    AND t.user_id = ?")
     Future<List<TaskEntity>> searchTaskOfUser(RoutingContext context, SqlConnection client, String taskName, BigInteger userId, Pageable pageable);
+
+    @Query("SELECT t.*\n" +
+            "FROM r_lcx.task t\n" +
+            "LEFT JOIN r_lcx.user u ON t.user_id = u.id\n" +
+            "WHERE t.task_name ILIKE ?\n" +
+            "    AND t.deleted_at IS NULL\n" +
+            "    AND t.user_id = ?")
+    Future<Page<TaskEntity>> searchTaskOfUserPage(RoutingContext context, SqlConnection client, String taskName, BigInteger userId, Pageable pageable);
 
     @Query("SELECT count(1)\n" +
             "FROM r_lcx.task t\n" +
