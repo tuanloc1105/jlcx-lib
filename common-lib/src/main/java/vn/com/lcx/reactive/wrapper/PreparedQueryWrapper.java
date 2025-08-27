905f2b138a21e51f22c327be657d30839930c6a0
tuanloc1105 <tuanloc1105@gmail.com>
2025-08-27 16:24:56 +0700

    feat: enhance PreparedQueryWrapper to handle enum values by storing their names

--- common-lib/src/main/java/vn/com/lcx/reactive/wrapper/PreparedQueryWrapper.java
+++ common-lib/src/main/java/vn/com/lcx/reactive/wrapper/PreparedQueryWrapper.java
@@ -64,7 +64,11 @@ public class PreparedQueryWrapper<T> implements PreparedQuery<T> {
                                     strValue(o)
                             )
                     );
-                    actualTuple.addValue(o);
+                    if (o.getClass().isEnum()) {
+                        actualTuple.addValue(((Enum<?>) o).name());
+                    } else {
+                        actualTuple.addValue(o);
+                    }
                 }
             } else {
                 parametersLog.append(
@@ -74,7 +78,11 @@ public class PreparedQueryWrapper<T> implements PreparedQuery<T> {
                                 strValue(value)
                         )
                 );
-                actualTuple.addValue(value);
+                if (value.getClass().isEnum()) {
+                    actualTuple.addValue(((Enum<?>) value).name());
+                } else {
+                    actualTuple.addValue(value);
+                }
             }
         }
         LogUtils.writeLog(context, LogUtils.Level.INFO, parametersLog.toString());
