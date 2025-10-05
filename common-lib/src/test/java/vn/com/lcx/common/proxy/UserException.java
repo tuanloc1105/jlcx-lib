16ec077bc9c3f16a5b6874563653c47eb70acb89
tuanloc1105 <tuanloc1105@gmail.com>
2025-10-05 13:20:05 +0700

    chore: update dependencies version

--- common-lib/src/test/java/vn/com/lcx/common/proxy/UserException.java
+++
@@ -1,9 +0,0 @@
-package vn.com.lcx.common.proxy;
-
-public class UserException extends RuntimeException {
-    private static final long serialVersionUID = 721200758187936524L;
-
-    public UserException(String message) {
-        super(message);
-    }
-}
